a678775bc75a1ade889a98ecb72df73963135932
LIPiston <wps1910093789@gmail.com>
2022-07-22 02:06:08 +0800

    修改了测试构建的脚本

--- dev-linux.sh
+++ dev-linux.sh
@@ -1,13 +1,17 @@
-echo ==========================================================================
-echo =
-echo =                          Frozen_Pear_Pack
-echo =
-echo =                          this pack:   
-echo =                                       LIPiston
-echo =                                       MouFengcoo
-echo =                                       LYOfficial
-echo =
-echo ==========================================================================
+echo "=============================================================================="
+echo " _____                           ____                  ____            _      "
+echo "|  ___| __ ___ _______ _ __     |  _ \ ___  __ _ _ __ |  _ \ __ _  ___| | __  "
+echo "| |_ | '__/ _ \_  / _ \  _ \    | |_) / _ \/ _  |  __|| |_) / _  |/ __| |/ /  "
+echo "|  _|| | | (_) / /  __/ | | |   |  __/  __/ (_| | |   |  __/ (_| | (__|   <   "
+echo "|_|  |_|  \___/___\___|_| |_|___|_|   \___|\__,_|_|___|_|   \__ _|\___|_|\_\  "
+echo "                          |_____|               |_____|                       "
+echo "                                                                              "
+echo "                           this pack:                                         "
+echo "                                       LIPiston                               "
+echo "                                       MouFengcoo                             "
+echo "                                       LYOfficial                             "
+echo "                                                                              "
+echo "=============================================================================="
 wget -q --show-progress https://github.com/LIPiston/Frozen_Pear_Pack/archive/refs/heads/master.zip
 mv master.zip dev.zip
 echo Please create pack.mcmeta into the dev.zip
